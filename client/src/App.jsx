d8f964e9a680069419cbdeaa13ce9be97a15ff51
Mohammed Afsal <mohammedafsaltut@gmail.com>
2024-02-29 07:06:00 +0530

    Switch between NewProject and NoProjectSelected components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,35 @@
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onAddProject={handleAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onAddProject={handleAddProject} />
+      {content}
     </main>
   );
 }
--- client/src/components/NoProjectSelected.jsx
+++ client/src/components/NoProjectSelected.jsx
@@ -1,7 +1,7 @@
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button.jsx";
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({ onAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
@@ -12,7 +12,7 @@ export default function NoProjectSelected() {
       <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
       <p className="text-stone-400 mb-4">Select a project or get started with new one</p>
       <p className="mt-8">
-        <Button>Create new project</Button>
+        <Button onClick={onAddProject}>Create new project</Button>
       </p>
     </div>
   );
